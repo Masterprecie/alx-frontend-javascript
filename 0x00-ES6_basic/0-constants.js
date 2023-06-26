8bdf2fa341b14847501edd41aa29e2699c4cc73f
Precious <ikpaprecious2@gmail.com>
2023-06-26 13:27:06 +0100

    fix

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,15 @@
-// Modify;
-//   Function taskFirst to instantiate variables using const
-// 	Function taskNext to instantiate variables using let
-
-
 export function taskFirst() {
-	const task = 'I prefer const when I can.';
-	return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getLast() {
-	return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-	return combination;
+  return combination;
 }
